9ae073ce09eed75f96e892947edb1f96f85329af
krzysztofkicinger <krzysztofkicinger@gmail.com>
2017-08-28 18:37:25 +0200

    Added body parser to the project

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
 const winston = require('winston');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (request, response) => {
+    response.send('Test response');
+});
+
+app.post('/', (request, response) => {
     response.send('Test response');
 });
 
