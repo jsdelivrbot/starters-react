9d45bd7730b1d14bf5cf03ff442b571a29cde128
krzysztofkicinger <krzysztofkicinger@gmail.com>
2017-08-28 18:58:04 +0200

    Added cookie parser to the project

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const morgan = require('morgan');
 const winston = require('winston');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.get('/', (request, response) => {
     response.send('Test response');
